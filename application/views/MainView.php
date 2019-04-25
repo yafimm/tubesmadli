0cce9e27d9913ca92d9302d009d34a519cdc3389
yafimm <9e.yafi.maulana@gmail.com>
2019-04-25 23:04:09 +0700

    First Comit

---
+++ application/views/MainView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="<?=base_url('assets/bootstrap/css/bootstrap.min.css')?>">
+  </head>
+  <body>
+    <h3>yeuh geus</h3>
+    <a href="google.com" class="btn btn-success">Bukti Button make class Bootstrap</a>
+  </body>
+</html>
